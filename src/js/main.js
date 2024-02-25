e78d0fa14bdfb24950228d18003e4ef53239869b
JunkyX1122 <122457074+JunkyX1122@users.noreply.github.com>
2024-02-25 22:36:12 +0000

    50 images

--- src/js/main.js
+++ src/js/main.js
@@ -106,7 +106,7 @@ function init() {
   document.querySelector('.image.selector').insertAdjacentElement('beforeend', document.createElement('select'));
 
   /** Initialize image quantity selector for results. */
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 0; i <= 50; i++) {
     const select = document.createElement('option');
     select.value = i;
     select.text = i;
